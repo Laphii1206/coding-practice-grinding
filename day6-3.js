507fd2db7203e54204ca8c14d5230dbc3167d3a3
Laphii <91127087+Laphii1206@users.noreply.github.com>
2025-10-20 21:48:57 +0800

    holy shit i cooked this

---
+++ day6-3.js
@@ -0,0 +1,24 @@
+var findMedianSortedArrays = (nums1, nums2) => {
+    let combined = nums1.concat(nums2);
+    let j = 0
+    let k = combined.length - 1;
+    combined.sort(function(a, b){return a - b});
+
+    if (combined.length % 2 == 1) {
+        return combined[parseInt(combined.length / 2)];
+    } else {
+
+        while (j < k) {
+            j++;
+            k--;
+        }
+
+        return (combined[j] + combined[k]) / 2.0;
+    }
+};
+
+let setA = [1,2,3,4,5]
+let setB = [6,7,8,9,10,11,12,13,14,15,16,17]
+
+
+console.log(findMedianSortedArrays(setA, setB))
